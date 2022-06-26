42e764e578b3562e29c9d9c27ff8a5b35417fd12
lady ada <limor@ladyada.net>
2022-06-25 23:00:02 -0400

    init

---
+++ adafruit_pcf8574.py
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2022 ladyada for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+"""
+`adafruit_pcf8574`
+================================================================================
+
+Python library for PCF8574 GPIO expander
+
+
+* Author(s): ladyada
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+* `Adafruit PCF8574 GPIO Expander <https://www.adafruit.com/product/5545>`
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+* Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+
+"""
+
+# imports
+
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_PCF8574.git"
